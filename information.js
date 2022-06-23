3f2e99c0472297c4ebc797b35afc3e15042d75eb
OloladeOjeniyi <105402862+OloladeOjeniyi@users.noreply.github.com>
2022-06-23 04:02:04 +0100

    Add files via upload

---
+++ information.js
@@ -0,0 +1,5 @@
+var name = "Ojeniyi Ololade Esther";
+var country = "Nigeria";
+var height = "5.6 ft.";
+
+alert("Hi, this is some information about me. \n\n Name: " + name + ".\n Height: " + height + "\n Country: " + country + "." );
